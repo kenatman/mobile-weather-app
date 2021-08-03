5de6491d7df7f0e9e1d13ed8b1e165a78e0cbd33
Yunsu <kenatman0723@gmail.com>
2021-08-03 12:15:48 +0900

    Getting Geolocation

--- App.jsx
+++ App.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Loading from "./loading";
+import React, { Component } from "react";
+import { Alert } from "react-native";
+import Loading from "./components/loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading />;
+class App extends Component {
+  state = { isLoading: true };
+  async getLocation() {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({});
+      //send  coords to API..
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can not find you`, `Check the permission!!");
+    }
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return isLoading && <Loading />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  yellowBox: { flex: 1, backgroundColor: "yellow" },
-  blueBox: { flex: 1, backgroundColor: "blue" },
-});
+export default App;
