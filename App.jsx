05f70c969db0eaf7d9f08c243d66551e2de568ab
Yunsu <kenatman0723@gmail.com>
2021-08-03 12:15:48 +0900

    Getting Weather

--- App.jsx
+++ App.jsx
@@ -2,9 +2,21 @@ import React, { Component } from "react";
 import { Alert } from "react-native";
 import Loading from "./components/loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = `01e2e9d8ce06e4275e9a0fd770444029`;
 
 class App extends Component {
   state = { isLoading: true };
+
+  async getWeather(lat, lon) {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather`,
+      { params: { lat, lon, appid: API_KEY } }
+    );
+    console.log(data);
+  }
+
   async getLocation() {
     try {
       await Location.requestForegroundPermissionsAsync();
@@ -12,6 +24,7 @@ class App extends Component {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync({});
       //send  coords to API..
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can not find you`, `Check the permission!!");
